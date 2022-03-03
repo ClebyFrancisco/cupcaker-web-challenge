a2100b62ccb640f312504b9fe718b185b0173439
Cleby Francisco <clebysilva5@gmail.com>
2022-03-03 09:57:03 -0300

    TASK-008_update-home-page

--- src/app/core/pages/home/index.tsx
+++ src/app/core/pages/home/index.tsx
@@ -1,16 +1,42 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-import { Panel, Typography, TypographyVariant } from 'components/atoms'
-import { EmptyState, EmptyStateType } from 'components/molecules'
+import { Button, Panel, Typography, TypographyVariant } from 'components/atoms'
+import { CoinList, EmptyState, EmptyStateType } from 'components/molecules'
 
+import { dataReturn } from '../AddNewCoin'
 import styles from './styles.module.scss'
 
+console.log(dataReturn)
+
 const Home = (): JSX.Element => {
+  const history = useHistory()
   return (
     <div className={styles.container}>
-      <Typography text="Watchlist" variant={TypographyVariant.h3} />
+      <div className={styles.containerTop}>
+        <Typography text="Watchlist" variant={TypographyVariant.h3} />
+        <Button
+          text="ADD NEW COIN"
+          type="button"
+          onclick={() => {
+            history.push('/new-coin')
+          }}
+        />
+      </div>
+
       <Panel className={styles.panel}>
-        <EmptyState type={EmptyStateType.emptyWallet} />
+        {dataReturn.length ? (
+          dataReturn.map(item => (
+            <CoinList
+              name={item.name}
+              date={item.date}
+              quantity={Number(item.quantity)}
+              price={item.price}
+            />
+          ))
+        ) : (
+          <EmptyState type={EmptyStateType.emptyWallet} />
+        )}
       </Panel>
     </div>
   )
